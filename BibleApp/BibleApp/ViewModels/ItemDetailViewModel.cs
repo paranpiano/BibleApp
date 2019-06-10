e2e27f3e44ce8973c4c4445c63ef9e718c426922
paranpiano <paran_piano@hotmail.com>
2019-06-10 15:08:41 +0900

    Add project files.

---
+++ BibleApp/BibleApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BibleApp.Models;
+
+namespace BibleApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
